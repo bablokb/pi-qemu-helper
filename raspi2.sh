1a93f0d58fdd551de56aeebcd562326c9dd669a5
Bernhard Bablok <bablokb@gmx.de>
2020-11-30 15:31:06 +0100

    moved implementation to raspi_.sh

--- raspi2.sh
+++ raspi2.sh
@@ -9,34 +9,9 @@
 #
 # --------------------------------------------------------------------------
 
-EXTRA_ARGS="bcm2708_fb.fbwidth=1280 bcm2708_fb.fbheight=1024 dwc_otg.fiq_fsm_enable=0"
+# Just call raspi_.sh with standard arguments. Adapt to your needs.
 
-osimg="$1"
-if [ -z "$osimg" ]; then
-  echo -e "usage: $0 image" >&2
-  exit 3
-fi
-workdir="$(dirname $osimg)"
-osimg="$(basename $osimg)"
-shift 1
+pgmdir=$(dirname "$0")
 
-# Aufruf: 1. Block: Hardware
-#         2. Block: Kernel, Kommandozeile, dtb
-#         3. Block: allgemeine QEmu-Optionen
+"$pgmdir"/raspi_.sh -b raspi2 "$@"
 
-qemu-system-arm \
-  -machine raspi2 \
-  -device usb-mouse \
-  -device usb-kbd \
-  -drive file="$workdir/$osimg",if=sd \
-  -netdev user,id=net0,hostfwd=tcp::8022-:22 \
-  -device usb-net,netdev=net0 \
-\
-  -kernel "$workdir/kernel7.img" \
-  -append "$(cat $workdir/cmdline.txt) $EXTRA_ARGS" \
-  -dtb "$workdir/bcm2709-rpi-2-b.dtb" \
-\
-  -no-reboot \
-  -daemonize \
-\
-  "${@}"
